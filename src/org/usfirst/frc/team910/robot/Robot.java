7a70a85ecd0958830d1767c88a7bfa6b1e700849
roboprog <roboprog@int.bishopfoley.edu>
2017-01-11 20:39:41 -0500

    added basic tank drive

--- src/org/usfirst/frc/team910/robot/Robot.java
+++ src/org/usfirst/frc/team910/robot/Robot.java
@@ -1,21 +1,17 @@
 package org.usfirst.frc.team910.robot;
 
+import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Joystick;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
-	final String defaultAuto = "Default";
-	final String customAuto = "My Auto";
-	String autoSelected;
-	SendableChooser<String> chooser = new SendableChooser<>();
+
+	Joystick leftJoy;
+	Joystick rightJoy;
+	CANTalon motorLF;
+	CANTalon motorRF;
+	CANTalon motorLR;
+	CANTalon motorRR;
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -23,28 +19,17 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void robotInit() {
-		chooser.addDefault("Default Auto", defaultAuto);
-		chooser.addObject("My Auto", customAuto);
-		SmartDashboard.putData("Auto choices", chooser);
+		leftJoy = new Joystick(0);
+		rightJoy = new Joystick(1);
+		motorLF = new CANTalon(0);
+		motorRF = new CANTalon(1);
+		motorLR = new CANTalon(2);
+		motorRR = new CANTalon(3);
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
 	@Override
 	public void autonomousInit() {
-		autoSelected = chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
+
 	}
 
 	/**
@@ -52,15 +37,12 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousPeriodic() {
-		switch (autoSelected) {
-		case customAuto:
-			// Put custom auto code here
-			break;
-		case defaultAuto:
-		default:
-			// Put default auto code here
-			break;
-		}
+
+	}
+
+	@Override
+	public void teleopInit() {
+
 	}
 
 	/**
@@ -68,6 +50,13 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
+		double leftPwr = leftJoy.getY();
+		double rightPwr = rightJoy.getY();
+
+		motorLF.set(leftPwr);
+		motorRF.set(rightPwr);
+		motorLR.set(leftPwr);
+		motorRR.set(rightPwr);
 	}
 
 	/**
@@ -75,6 +64,6 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void testPeriodic() {
+
 	}
 }
-
