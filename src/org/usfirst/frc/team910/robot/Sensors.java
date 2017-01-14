7e553ba64223c3097423b6e1da529651f35617de
roboprog <roboprog@int.bishopfoley.edu>
2017-01-14 12:23:38 -0500

    added encoders to sensors file

--- src/org/usfirst/frc/team910/robot/Sensors.java
+++ src/org/usfirst/frc/team910/robot/Sensors.java
@@ -1,5 +1,24 @@
 package org.usfirst.frc.team910.robot;
 
+import edu.wpi.first.wpilibj.Encoder;
+
 public class Sensors {
 
+	private Encoder lEncoder;
+	private Encoder rEncoder;
+
+	public double leftEncoder;
+	public double rightEncoder;
+
+	Sensors() {
+		lEncoder = new Encoder(ElectroPaul.LEFT_ENCODER_PORT_1, ElectroPaul.LEFT_ENCODER_PORT_2, false);
+		rEncoder = new Encoder(ElectroPaul.RIGHT_ENCODER_PORT_1, ElectroPaul.RIGHT_ENCODER_PORT_2, false);
+		lEncoder.setDistancePerPulse(1);
+		rEncoder.setDistancePerPulse(1);
+	}
+
+	public void read() {
+		leftEncoder = lEncoder.get();
+		rightEncoder = rEncoder.get();
+	}
 }
