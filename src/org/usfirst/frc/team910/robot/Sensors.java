fc287fd3a9f3040f735977388d93916ed0fd8b21
roboprog <roboprog@int.bishopfoley.edu>
2017-01-14 15:39:43 -0500

    Added navx to sensors

--- src/org/usfirst/frc/team910/robot/Sensors.java
+++ src/org/usfirst/frc/team910/robot/Sensors.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team910.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -9,21 +11,28 @@ public class Sensors {
 
 	private Encoder lEncoder;
 	private Encoder rEncoder;
+	private AHRS navx;
 
 	public double leftEncoder;
 	public double rightEncoder;
-
+	public double robotAngle;
 	Sensors() {
 		lEncoder = new Encoder(ElectroPaul.LEFT_ENCODER_PORT_1, ElectroPaul.LEFT_ENCODER_PORT_2, false);
 		rEncoder = new Encoder(ElectroPaul.RIGHT_ENCODER_PORT_1, ElectroPaul.RIGHT_ENCODER_PORT_2, true);
 		lEncoder.setDistancePerPulse(ENCODER_RESOLUTION);
 		rEncoder.setDistancePerPulse(ENCODER_RESOLUTION);
+		navx = new AHRS(SPI.Port.kMXP);
 	}
 
 	public void read() {
 		leftEncoder = lEncoder.getDistance();
 		rightEncoder = rEncoder.getDistance();
+		robotAngle = navx.getYaw();
 		SmartDashboard.putNumber("leftdistance", leftEncoder);
 		SmartDashboard.putNumber("rightdistance", rightEncoder);
+		SmartDashboard.putNumber("navxYaw", robotAngle);
+		
+		
 	}
+
 }
