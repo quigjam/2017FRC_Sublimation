805f466cdbb41382c12e0163fa2b9cb1739c70e6
roboprog <roboprog@int.bishopfoley.edu>
2017-01-21 15:08:56 -0500

    Continued WIP on algo for turning in to gear peg

--- src/org/usfirst/frc/team910/robot/PathPlanning.java
+++ src/org/usfirst/frc/team910/robot/PathPlanning.java
@@ -9,10 +9,11 @@ public class PathPlanning {
 		public double yIntercept = 0;
 	}
 
-	
-	
-	
-	
+	private class Point {
+		public double x;
+		public double y;
+	}
+
 	double sensorsNavXangle = 0;
 	int inputsTargetGearPost = 0;
 	double inputsCameraAngle = 0;
@@ -20,8 +21,14 @@ public class PathPlanning {
 
 	Line robotLine = new Line();
 	Line goalLine = new Line();
-	Line parallelrobotLine = new Line(); //previously X
-	Line parallelgoalLine = new Line();  //previously Y
+	Line parallelrobotLine = new Line(); // previously X
+	Line parallelgoalLine = new Line(); // previously Y
+	Line perpendicularrobotLine = new Line();
+	Line perpendiculargoalLine = new Line();
+
+	Point centerofCircle = new Point();
+	Point goalIntersection = new Point();
+	Point robotIntersection = new Point();
 
 	private void calculateArcPoints() {
 		// Step 1 Line from Start in current Direction
@@ -53,18 +60,36 @@ public class PathPlanning {
 		// Step 3 Generate parallel inboard lines
 		parallelrobotLine.slope = robotLine.slope;
 		parallelgoalLine.slope = goalLine.slope;
+
 		parallelrobotLine.yIntercept = CIRCLE_RADIUS / Math.cos(Math.toRadians(sensorsNavXangle));
 		parallelgoalLine.yIntercept = CIRCLE_RADIUS / Math.cos(Math.toRadians(inputsCameraAngle));
 
-		//Step 4, find center of circle
-		//set the parrallelrobotline and parrallelgoalline equal to each other to find the x intercept they have in common
-		
-		//Step 5, perpandicular lines from the center of the circle to the orginal lines
-		//add the y-interecept of of the parrallel robot line to the Circle_Radius to get to the orginal robot line
-		//add the y-intercept of the parrallel goal line to the Circle_Radius to get to the orginal goal line
-		
-		//Step 6 find the coordinates of the intercetion of the perpandicular lines from the center of the circle to the orginal lines.
-		//
+		// Step 4, find center of circle
+		// set the parallelrobotline and parallelgoalline equal to each other to
+		// find the 1 point they have in common
+		centerofCircle.x = (parallelgoalLine.yIntercept - parallelrobotLine.yIntercept)
+				/ (parallelrobotLine.slope - parallelgoalLine.slope);
+		centerofCircle.y = parallelrobotLine.slope * centerofCircle.x + parallelrobotLine.yIntercept;
+		// Step 5, perpendicular lines from the center of the circle to the
+		// original lines
+		// perpendicular slopes are the negative inverse (-1/m)
+		perpendicularrobotLine.slope = -1 / parallelrobotLine.slope;
+		perpendiculargoalLine.slope = -1 / parallelgoalLine.slope;
+		// we know the line passes through the center of the circle and has a
+		// slope of (-1/m)
+		// solve for y-intercept
+		perpendicularrobotLine.yIntercept = centerofCircle.y - (perpendicularrobotLine.slope * centerofCircle.x);
+		perpendiculargoalLine.yIntercept = centerofCircle.y - (perpendiculargoalLine.slope * centerofCircle.x);
+		// Step 6 find the coordinates of the intersection of the perpendicular
+		// lines from the center of the circle to the original lines.
+		// these intersections are the start and end points of our arc
+		goalIntersection.x = (perpendiculargoalLine.yIntercept - goalLine.yIntercept)
+				/ (goalLine.slope - perpendiculargoalLine.slope);
+		goalIntersection.y = (perpendiculargoalLine.slope * goalIntersection.x) + perpendiculargoalLine.yIntercept;
+
+		robotIntersection.x = (perpendicularrobotLine.yIntercept - robotLine.yIntercept)
+				/ (robotLine.slope - perpendicularrobotLine.slope);
+		robotIntersection.y = (perpendicularrobotLine.slope * robotIntersection.x) + perpendicularrobotLine.yIntercept;
 	}
 
 }
