f9131f4985c8438446624329cf23d5fcc8b4bb28
roboprog <roboprog@int.bishopfoley.edu>
2017-02-18 16:15:38 -0500

    Updated and finished AutoClimb

--- src/org/usfirst/frc/team910/robot/Functions/AutoClimb.java
+++ src/org/usfirst/frc/team910/robot/Functions/AutoClimb.java
@@ -5,14 +5,19 @@ import org.usfirst.frc.team910.robot.IO.Outputs;
 import org.usfirst.frc.team910.robot.IO.Sensors;
 import org.usfirst.frc.team910.robot.Subsystems.Climber;
 import org.usfirst.frc.team910.robot.Subsystems.DriveTrain;
+import org.usfirst.frc.team910.robot.Vision.Target;
 
-
+import edu.wpi.first.wpilibj.Timer;
 
 public class AutoClimb {
-	
+
 	private static final double DRIVE_POWER = 0.2;
 	private static final double ALLOWABLE_ANGLE_ERROR = .5;
-	
+	private static final double LAG_TIME = 0.1;
+	private static final double ALLOWABLE_DISTANCE_ERROR = 1;
+	private static final double CLIMB_POWER = 0.9;
+	private static final double DRIVE_TIME = 0.5; 
+
 	private Inputs in;
 	private Outputs out;
 	private Sensors sense;
@@ -27,38 +32,45 @@ public class AutoClimb {
 	}
 
 	private enum ClimbState {
-		CAM_CHECK, DRIVE, ALIGN, CLIMB
+		CAM_CHECK, DRIVE, DRIVE2, CLIMB
 	};
 
 	private ClimbState climbState = ClimbState.values()[0];
+	private Target currentTarget;
+	private double endTime = 0;  
 
 	public void run() {
 		if (in.autoClimb) {
+			currentTarget = sense.camera.rope.getCurrentTarget();
 			switch (climbState) {
 			case CAM_CHECK:
 
-				if (sense.camera.climbSearch()) { // fix later
+				if (Timer.getFPGATimestamp() - currentTarget.time < LAG_TIME) {
 					climbState = ClimbState.DRIVE;
 
 				}
 				break;
-				
+
 			case DRIVE:
 				drive.originAngle.set(sense.robotAngle.get() + sense.cameraAngle.get());
 				drive.driveStraightNavX(false, DRIVE_POWER, 0);
-				if (true) { // when camera class is done we will get the distance of the camera to be = 0					
-					climbState = ClimbState.ALIGN;
+				if ((currentTarget.distance) < ALLOWABLE_DISTANCE_ERROR) {
+					climbState = ClimbState.DRIVE2;
+					endTime = Timer.getFPGATimestamp() + DRIVE_TIME;  
 				}
-			
-			case ALIGN:
-				drive.rotate(sense.cameraAngle);
-				if (Math.abs(sense.cameraAngle.get()) < ALLOWABLE_ANGLE_ERROR) {
-				climbState = ClimbState.CLIMB;
+				break;
+			case DRIVE2:
+				drive.originAngle.set(sense.robotAngle.get() + sense.cameraAngle.get());
+				drive.driveStraightNavX(false, DRIVE_POWER, 0);
+				climb.forward(CLIMB_POWER);
+				if(Timer.getFPGATimestamp() > endTime) { 
+				climbState = climbState.CLIMB;
 				}
 				break;
 
 			case CLIMB:
-			// climb.climbNow(0);// TODO see if number is needed here
+
+				climb.forward(CLIMB_POWER);
 			}
 
 		}
