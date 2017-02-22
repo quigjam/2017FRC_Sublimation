f10d884dad318d55968b94228e0e7f4f1ef938ae
roboprog <roboprog@int.bishopfoley.edu>
2017-02-21 21:20:55 -0500

    added ability for the practice and competition bots to have different IO

--- src/org/usfirst/frc/team910/robot/Robot.java
+++ src/org/usfirst/frc/team910/robot/Robot.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team910.robot;
 import org.usfirst.frc.team910.robot.Functions.AutoClimb;
 import org.usfirst.frc.team910.robot.Functions.AutoGear;
 import org.usfirst.frc.team910.robot.Functions.AutoShoot;
+import org.usfirst.frc.team910.robot.IO.ElectroPaul;
 import org.usfirst.frc.team910.robot.IO.Inputs;
 import org.usfirst.frc.team910.robot.IO.Outputs;
 import org.usfirst.frc.team910.robot.IO.Sensors;
@@ -33,6 +34,7 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void robotInit() {
+		ElectroPaul ep = new ElectroPaul();
 		in = new Inputs();
 		out = new Outputs();
 		sense = new Sensors();
